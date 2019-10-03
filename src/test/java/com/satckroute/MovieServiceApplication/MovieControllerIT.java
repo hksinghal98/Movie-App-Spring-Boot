cb8da3cf5ecdb39f35c680849911e29c9a85e6dd
himanshu.singhal <himanshu.singhal@cgi.com>
2019-10-03 12:41:27 +0530

    Task2 done

---
+++ src/test/java/com/satckroute/MovieServiceApplication/MovieControllerIT.java
@@ -0,0 +1,14 @@
+package com.satckroute.MovieServiceApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+public class MovieControllerIT {
+
+    @Test
+    public void dummyTest(){
+        System.out.println("Movie Controller IT test passed");
+    }
+}
