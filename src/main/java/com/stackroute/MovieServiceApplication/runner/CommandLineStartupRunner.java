ba22eee310a3565811a5d6ece495165dd77a34ce
himanshu.singhal <himanshu.singhal@cgi.com>
2019-10-03 16:48:48 +0530

    Task6 done

---
+++ src/main/java/com/stackroute/MovieServiceApplication/runner/CommandLineStartupRunner.java
@@ -0,0 +1,29 @@
+package com.stackroute.MovieServiceApplication.runner;
+
+import com.stackroute.MovieServiceApplication.customException.MovieAlreadyExistException;
+import com.stackroute.MovieServiceApplication.domain.Movie;
+import com.stackroute.MovieServiceApplication.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineStartupRunner implements CommandLineRunner {
+    @Autowired
+    private MovieService movieService;
+
+    public CommandLineStartupRunner(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("invoked on startup");
+        Movie movie = new Movie(2,"WARfkhsdk",(float) 7.3,"Action Movie","02/10/2019");
+        try {
+            movieService.saveMovie(movie);
+        } catch (MovieAlreadyExistException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/stackroute/MovieServiceApplication/runner/SpringContextListener.java
@@ -0,0 +1,30 @@
+package com.stackroute.MovieServiceApplication.runner;
+
+import com.stackroute.MovieServiceApplication.customException.MovieAlreadyExistException;
+import com.stackroute.MovieServiceApplication.domain.Movie;
+import com.stackroute.MovieServiceApplication.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextListener implements ApplicationListener<ContextRefreshedEvent> {
+    @Autowired
+    private MovieService movieService;
+
+    public SpringContextListener(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        System.out.println("invoked on startup");
+        Movie movie = new Movie(1,"WAR",(float) 7.3,"Action Movie","02/10/2019");
+        try {
+            movieService.saveMovie(movie);
+        } catch (MovieAlreadyExistException e) {
+            e.printStackTrace();
+        }
+    }
+}
